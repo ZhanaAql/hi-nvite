0d5bbc077005a450c0a637ad00d9ab4565526a2d
zhanaaql <zulfahanaaqliyah@gmail.com>
2024-07-22 14:18:05 +0700

    edit footer

--- src/page/Footer.jsx
+++ src/page/Footer.jsx
@@ -1,30 +1,28 @@
 import React from 'react'
 import { FaWhatsapp } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 export default function Footer() {
   return (
     <div className='footer' id='about'>
-           <div className="container">
-        <div className="sosmed">
-          <div className="wrapper">
-            <div className="icon">
-              <span className="tooltip">Whatsapp</span>
-              <a href="https://wa.me/message/2WUZVJ6YA27BK1" target='_blank'><FaWhatsapp className='footer-icon'/></a>
-            </div>
-            {/* <div className="icon">
-              <span className="tooltip">Github</span>
-              <a href="https://github.com/ZhanaAql" target='_blank'><FaGithub className='footer-icon'/></a>
-            </div>
-            <div className="icon">
-              <span className="tooltip">LinkedIn</span>
-              <a href="https://www.linkedin.com/in/zulfa-hana-aqliyah-592933298/" target='_blank'><FaLinkedin className='footer-icon'/></a>
-            </div> */}
+      <div className="container">
+        <div className="footer-info">
+            <span className="footer-logo" style={{ fontFamily: "Sacramento,  cursive"}}>Hi-nvite</span>
+            <div className="sosmed">
+                <div className="icon">
+                  <a href="https://wa.me/message/2WUZVJ6YA27BK1" target='_blank'><FaWhatsapp className='footer-icon'/></a>
+                </div>
+                <div className="icon">
+                  <a href="" target='_blank'><FaFacebook className='footer-icon'/></a>
+                </div>
+                <div className="icon">
+                  <a href="" target='_blank'><FaInstagram className='footer-icon'/></a>
+                </div>
+              </div>
           </div>
-        </div>
-        <div className="footer-credit">
-          <p>Hi-Nvite ©2024 — All Rights Reserved</p>
-        </div>
+          <div className="footer-credit">
+               <p>Copyright ©2024 All Rights Reserved</p>
+          </div>
       </div>
     </div>
   )
